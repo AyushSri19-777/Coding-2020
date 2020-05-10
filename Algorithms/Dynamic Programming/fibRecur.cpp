b5698119e0f56e79df05b294119c376b4edf9290
Ayush Srivastava <sri.ayush777@gmail.com>
2020-05-10 22:22:50 +0530

    Recursive fibo added

---
+++ Algorithms/Dynamic Programming/fibRecur.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int fib(int n)
+{
+    if(n==1)
+        return 0;
+    if(n==2)
+        return 1;
+    else
+        return fib(n-1)+fib(n-2);
+}
+int main()
+{
+    cout<<"Enter n= ";
+    int n;
+    cin>>n;
+    cout<<"nth fibonacci number is= "<<fib(n);
+}

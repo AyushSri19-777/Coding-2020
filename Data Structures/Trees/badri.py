0cda02907840f80aba7ecceaa5cd4634d66dd553
Ayush Srivastava <sri.ayush777@gmail.com>
2020-05-15 00:08:28 +0530

    gvg

--- Data Structures/Trees/badri.py
+++ Data Structures/Trees/badri.py
@@ -1,10 +1,10 @@
 def badri():
     sum=0
     for i in range(1000):
-        k=int(input("agla hisab"))
+        k=int(input("agla hisab "))
         if k==-1:
             break
         sum=sum+k
-    print("total cost",sum)
+    print("total cost= ",sum)
 
 badri()

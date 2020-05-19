d9b0f367543b06206d9b3af93b9557bce2884eb0
Ayush Srivastava <sri.ayush777@gmail.com>
2020-05-19 23:04:55 +0530

    sjk

--- Data Structures/Trees/minValue.cpp
+++ Data Structures/Trees/minValue.cpp
@@ -51,7 +51,7 @@ Link- https://practice.geeksforgeeks.org/problems/minimum-element-in-bst/1/
 int minValue(Node* root )
 {
     if(root == NULL)
-        return -1;
+        return -1 ;
     if(root->left==NULL)
         return root->data;
     minValue(root->left);

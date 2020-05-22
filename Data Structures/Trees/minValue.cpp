a0abc88852f5cc93fbb548a222af76b6f0b6318b
Ayush Srivastava <sri.ayush777@gmail.com>
2020-05-22 23:15:42 +0530

    uih

--- Data Structures/Trees/minValue.cpp
+++ Data Structures/Trees/minValue.cpp
@@ -52,7 +52,7 @@ int minValue(Node* root )
 {
     if(root == NULL)
         return -1 ;
-    if(root->left==NULL)
+    if(root->left== NULL)
         return root->data;
     minValue(root->left) ;
 }

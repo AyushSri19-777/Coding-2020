6d880605e7f2de76276b70d96858fe6abc63213a
Ayush Srivastava <sri.ayush777@gmail.com>
2020-06-07 23:04:03 +0530

    rod cutting problem

--- Algorithms/Dynamic Programming/RodCutting.cpp
+++ Algorithms/Dynamic Programming/RodCutting.cpp
@@ -1,3 +1,25 @@
+/*Given a rod of length n inches and an array of prices that contains prices of all pieces of size smaller than n. Determine the maximum value obtainable by cutting up the rod and selling the pieces.
+
+Input:
+First line consists of T test cases. First line of every test case consists of n, denoting the size of array. Second line of every test case consists of price of ith length piece.
+
+Output:
+For each testcase, in a new line, print a single line output consists of maximum price obtained.
+
+Constraints:
+1 <= T <= 100
+1 <= n <= 100
+1 <= Ai <= 100
+
+Example:
+Input:
+1
+8
+1 5 8 9 10 17 17 20
+Output:
+22
+Link-https://practice.geeksforgeeks.org/problems/rod-cutting/0
+*/
 #include <iostream>
 #include <climits>
 #include <math.h>

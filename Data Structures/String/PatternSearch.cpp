2910628a9924e5e65a3fe2a93f12728bf823e944
Ayush Srivastava <sri.ayush777@gmail.com>
2020-07-17 22:00:47 +0530

    Rabin Karp done

---
+++ Data Structures/String/PatternSearch.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+int getHash(string s)
+{
+    unordered_map <char,int> U;
+    int sum=0;
+    for(int i=1;i<=26;i++)
+    {
+        U[char(i+96)]=i;
+    }
+
+    for(int i=0;i<s.length();i++)
+    {
+        sum+=U[s[i]]*pow(10,s.length()-i);
+    }
+    return sum;
+}
+int main()
+{
+    string s,p,w;
+    cout<<"Enter the string and pattern\n";
+    cin>>s>>p;
+    int pat=getHash(p),count=0,i;
+    cout<<s.substr(0,3)<<"char\n";
+    for(int j=0;j<s.length()-2;j++)
+    {
+        w=s.substr(j,3);
+        cout<<w<<"\t"<<getHash(w)<<"\n";
+        if(pat==getHash(w))
+            count++;
+    }
+    cout<<"No. of pattern matches= "<<count;
+}

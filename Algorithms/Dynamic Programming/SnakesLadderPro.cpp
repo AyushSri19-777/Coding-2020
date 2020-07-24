91926974fe88339b6dcd0269c7fd6068d87df9ce
Ayush Srivastava <sri.ayush777@gmail.com>
2020-07-24 12:16:53 +0530

    s and l

---
+++ Algorithms/Dynamic Programming/SnakesLadderPro.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int m,n;
+    unordered_map<int,int> U;
+    cout<<"Enter matrix size";
+    cin>>n;
+    cout<<"Enter the max value";
+    cin>>m;
+    cout<<"Enter moves";
+    
+}

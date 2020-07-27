0c3b30ea2a709eaa731fc44f9854b040ce03f3ce
Ayush Srivastava <sri.ayush777@gmail.com>
2020-07-27 22:23:07 +0530

    kmsdm

--- Algorithms/Dynamic Programming/SnakesLadderPro.cpp
+++ Algorithms/Dynamic Programming/SnakesLadderPro.cpp
@@ -1,13 +1,34 @@
 #include<bits/stdc++.h>
 using namespace std;
+bool checkLadder(int i,int j,unordered_map U)
+{
+    if(U[i]<=j)
+        return true;
+    return false;
+}
+int minMoves(int start,int end,int i,int j,unordered_map U)
+{
+    if(start==n)
+        return 0;
+    if(checkLadder(i,j,U)==true)
+        start+=U[i];
+}
 int main()
 {
-    int m,n;
+    int m,n,h;
     unordered_map<int,int> U;
     cout<<"Enter matrix size";
     cin>>n;
     cout<<"Enter the max value";
     cin>>m;
     cout<<"Enter moves";
-    
+    cin>>h;
+    for(int i=0;i<h;h++)
+    {
+        int x,y;
+        cin>>x>>y;
+        U[x]=y;
+    }
+    cout<<minMoves()
+
 }
